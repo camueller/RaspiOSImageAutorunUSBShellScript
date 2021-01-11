61177f0972acce56366d59d1ed9887500a641cf0
Axel Müller <axel.mueller@avanux.de>
2021-01-11 19:26:29 +0100

    only execute the first script after sorting

--- usbScriptRunner.sh
+++ usbScriptRunner.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-SELF_FILE=${0##*/}
-INSTALL_DIR=/tmp/${SELF_FILE%.*}
+MOUNT_POINT=/media
+INSTALL_DIR=/tmp
 ACTION=$1
 DEVICE=$2
 POWER_LED=/sys/class/leds/led1/brightness
@@ -8,11 +8,10 @@ POWER_LED=/sys/class/leds/led1/brightness
 case $ACTION in
   "add")
      echo 255 > $POWER_LED
-     mount /dev/$DEVICE /media
-     mkdir $INSTALL_DIR
-     cp /media/*.sh $INSTALL_DIR
-     chmod +x $INSTALL_DIR/*.sh
-     find $INSTALL_DIR -name "*.sh" -exec {} \;
+     mount /dev/$DEVICE $MOUNT_POINT
+     RUN_SCRIPT="$(basename `find $MOUNT_POINT -name "*.sh" | sort | head -n 1`)"
+     cp $MOUNT_POINT/$RUN_SCRIPT $INSTALL_DIR
+     $INSTALL_DIR/$RUN_SCRIPT
      echo 0 > $POWER_LED
      ;;
    "remove")
