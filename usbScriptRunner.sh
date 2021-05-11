e07b078596a9a023e5d6aa58d15f37ec4dddef0b
Axel Müller <axel.mueller@avanux.de>
2021-05-11 12:06:56 +0200

    fix crlf for Windows

--- usbScriptRunner.sh
+++ usbScriptRunner.sh
@@ -10,7 +10,8 @@ case $ACTION in
      echo 255 > $POWER_LED
      mount /dev/$DEVICE $MOUNT_POINT
      RUN_SCRIPT="$(basename `find $MOUNT_POINT -name "*.sh" | sort | head -n 1`)"
-     cp $MOUNT_POINT/$RUN_SCRIPT $INSTALL_DIR
+     cat $MOUNT_POINT/$RUN_SCRIPT | tr -d '\r' > $INSTALL_DIR/$RUN_SCRIPT
+     chmod +x $INSTALL_DIR/$RUN_SCRIPT
      $INSTALL_DIR/$RUN_SCRIPT
      echo 0 > $POWER_LED
      ;;
